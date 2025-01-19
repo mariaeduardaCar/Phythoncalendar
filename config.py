4fd31501022723f446665e68f731c0d8e81c4582
mariaeduardaCar <SeuEmailDeGitHub>
2025-01-19 00:46:16 -0300

    Configuração do banco de dados PostgreSQL

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
 # Configuração do banco de dados PostgreSQL
-DB_HOST = 'localhost'
-DB_NAME = 'agenda'
-DB_USER = 'postgres'
-DB_PASSWORD = 'Fe151206'
+DB_HOST = 'cfls9h51f4i86c.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com'
+DB_NAME = 'dda3grp6j34cq5'
+DB_USER = 'ubrehgbs0ep4pf'
+DB_PASSWORD = 'p5e595ef820ba0374e627e8e9430c975e6c2c864b761389438fa158540ba93710'
 
 # Caminho para o arquivo de credenciais do Google
 GOOGLE_CALENDAR_CREDENTIALS = 'credenciais.json'
